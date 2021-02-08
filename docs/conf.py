05f8bc6e92378f0f4199fce9342854555902d65b
chadpeters <chad.peters@carleton.ca>
2021-02-08 10:48:43 -0500

    napolean and MD support extensions for Sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ author = 'Chad Peters'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 
-#extensions = ['recommonmark']
+extensions = ['recommonmark', 'napoleon']
 
 
 # Add any paths that contain templates here, relative to this directory.
