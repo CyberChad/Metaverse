7c1c25aa99ac80fe928f87cb8dbee6ca382b45fd
chadpeters <chad.peters@carleton.ca>
2021-02-08 10:53:21 -0500

    sphinx napolean config

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,22 @@ author = 'Chad Peters'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 
-extensions = ['recommonmark', 'napoleon']
+extensions = ['recommonmark', 'sphinxcontrib.napoleon']
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = False
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_use_keyword = True
+napoleon_custom_sections = None
 
 
 # Add any paths that contain templates here, relative to this directory.
