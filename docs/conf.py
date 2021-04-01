1ea1d42f85574573c0dac6a4818b37f86c53265c
Chad Peters <chad.peters@carleton.ca>
2021-04-01 14:27:57 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@ import os
 import sys
 import time
 path = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.join(path, '..'))
 sys.path.insert(0, os.path.join(path, '..', '..'))
 #import metaverse
 
